5644b6cdd4684c947bf06237da17f4217ff5da77
Tzu-ping Chung <uranusjr@gmail.com>
2015-02-26 14:30:56 +0800

    Fix crash and info format in demo

--- demo/controller.cpp
+++ demo/controller.cpp
@@ -1,34 +1,51 @@
-#include "controller.h"
 #include <QDebug>
 #include <QIODevice>
+#include "controller.h"
 #include "device.h"
 
 
 Controller::Controller(QObject *parent) :
-    QObject(parent), count(0)
+    QObject(parent), handle(0), io(0), count(0)
 {
     QList<QUSB::Device> devices = QUSB::Device::availableDevices();
     foreach (const QUSB::Device &device, devices)
     {
         qDebug() << "Got device at:" << device.bus() << ":" << device.address();
-        qDebug() << "Device info:"<< "Product ID" << device.productId();
+        qDebug() << "Device info:";
+        qDebug() << "\tProduct ID" << device.productId();
 
         handle = QUSB::Handle::fromVendorIdProductId(
                     device.vendorId(), device.productId());
-        qDebug() << "            " << "Product description" <<
+        qDebug() << "\tProduct description" <<
                     handle->stringDescriptor(device.product());
-        qDebug() << "            " << "Manufacturer" <<
+        qDebug() << "\tManufacturer" <<
                     handle->stringDescriptor(device.manufacturer());
     }
-    handle = QUSB::Handle::fromVendorIdProductId(0xeb1a, 0x299f);
-    handle->setParent(this);
-    handle->claimInterface(0);
-    io = new QUSB::BulkIO(handle, 3, this);
-    io->open(QIODevice::ReadOnly);
-    connect(
-        io, SIGNAL(bytesReceived(QByteArray)),
-        this, SLOT(processBytes(QByteArray))
-    );
+
+    // Modify these to suit your needs.
+    quint16 vendor = 0xeb1a;
+    quint16 product = 0x299f;
+
+    handle = QUSB::Handle::fromVendorIdProductId(vendor, product);
+    if (handle)
+    {
+        handle->setParent(this);
+        handle->claimInterface(0);
+        io = new QUSB::BulkIO(handle, 3, this);
+    }
+    else
+    {
+        qDebug() << QString("Could not open device with ID: 0x%1, 0x%2")
+                    .arg(vendor, 4, 16, QChar('0'))
+                    .arg(product, 4, 16, QChar('0'));
+    }
+
+    if (io)
+    {
+        io->open(QIODevice::ReadOnly);
+        connect(io, SIGNAL(bytesReceived(QByteArray)),
+                this, SLOT(processBytes(QByteArray)));
+    }
 }
 
 Controller::~Controller()
